0313fb5f7fb7ba91cbd4915b5643ba7fa7adec88
brightvivek <41038863+brightvivek@users.noreply.github.com>
2018-12-28 11:36:37 +0530

    Create p38.py

---
+++ p38.py
@@ -0,0 +1,18 @@
+def max():
+   	a=int(input())
+   	b=int (input())
+   	c=int(input())
+   	if (a>b):
+      if(a>c):
+    			print (a);
+   		else :
+    			print(c);
+   	elif (b>c):
+   		print (b)
+   	else:
+   		print (c);
+def main():
+   	try:
+    		max()
+   	except ValueError:
+    		print('invalid);
