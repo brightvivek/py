dce0b2afcafcfb98671feea70e899975b2fa4e91
brightvivek <41038863+brightvivek@users.noreply.github.com>
2019-01-07 15:12:08 +0530

    Create p55.py

---
+++ p55.py
@@ -0,0 +1,13 @@
+def oddeven(n):
+        if n%2==0:
+                print("even");
+        else:
+                print("odd");
+def mul2():
+        try:
+                m=int(input())
+                n=int(input())
+                oddeven(m*n)
+        except:
+                print('invalid');
+
