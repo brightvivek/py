ad412d3cd618509e8b5e3b4ef306cee12696cc9c
brightvivek <41038863+brightvivek@users.noreply.github.com>
2019-01-07 15:05:04 +0530

    Create p54.py

---
+++ p54.py
@@ -0,0 +1,11 @@
+
+def main():
+        try:
+                n=int(input())
+                if n%2!=0:
+                        n-=1
+                print(n)
+        except:
+                print('invalid');
+main()
+
